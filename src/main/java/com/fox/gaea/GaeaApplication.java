c80451270af44a38d47db561e87d95163c6d9b43
archfox <dustin.rasch@outlook.de>
2023-08-25 15:43:03 +0200

    test action

--- src/main/java/com/fox/gaea/GaeaApplication.java
+++ src/main/java/com/fox/gaea/GaeaApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GaeaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("Run Git Action!");
 		SpringApplication.run(GaeaApplication.class, args);
 	}
 
